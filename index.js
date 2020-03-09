11cf078da4cb533a1e9c3e27f9cc035c3d6afc21
Strahinja <strahinjapantovic@ymail.com>
2020-03-09 17:58:36 +0100

    favicon from node server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //Install express server
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile("logo.png", "/dist", err => {
+  if (err) throw err;
+  console.log("source.txt was copied to destination.txt");
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist"));
